1a584044463b95deadbf28653988cc21dc38977e
Ronny Walter <mail@ronnywalter.de>
2022-07-30 10:46:10 +0200

    added query by id

--- src/main/java/de/ronnywalter/eve/sde/controller/MarketGroupController.java
+++ src/main/java/de/ronnywalter/eve/sde/controller/MarketGroupController.java
@@ -3,10 +3,12 @@ package de.ronnywalter.eve.sde.controller;
 import com.google.common.collect.Lists;
 import de.ronnywalter.eve.dto.MarketGroupDTO;
 import de.ronnywalter.eve.sde.model.EveIcon;
+import de.ronnywalter.eve.sde.model.InvMarketGroup;
 import de.ronnywalter.eve.sde.repository.EveIconRepository;
 import de.ronnywalter.eve.sde.repository.MarketGroupRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,15 +29,27 @@ public class MarketGroupController extends AbstractController{
         dtos.forEach(dto -> {
             Integer iconId = dto.getIconId();
             if(iconId != null) {
-                EveIcon icon = eveIconRepository.getById(dto.getIconId());
-
-                String fileName = icon.getIconFile().substring(icon.getIconFile().lastIndexOf("/") + 1);
-                dto.setIcon(fileName);
-
+                dto.setIcon(getIconFileName(iconId));
             }
         });
-
         return dtos;
     }
 
+    @GetMapping("/{id}")
+    public MarketGroupDTO getMarketGroup (@PathVariable int id) {
+        MarketGroupDTO dto = map(marketGroupRepository.findById(id).get(), MarketGroupDTO.class);
+
+        Integer iconId = dto.getIconId();
+        if(iconId != null) {
+            dto.setIcon(getIconFileName(iconId));
+        }
+        return dto;
+    }
+
+    private String getIconFileName(int iconId) {
+        EveIcon icon = eveIconRepository.getById(iconId);
+
+        String fileName = icon.getIconFile().substring(icon.getIconFile().lastIndexOf("/") + 1);
+        return fileName;
+    }
 }
